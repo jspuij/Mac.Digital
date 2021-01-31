59854bb3eaf0871d0a682979ccfedcf5859add35
jspuij <jws@birdseyeview.nl>
2021-01-31 22:39:50 +0100

    css

---
+++ src/Mac.Digital/webpack.mix.js
@@ -0,0 +1,30 @@
+﻿const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix
+    .ts("ts/index.ts", "wwwroot/js")
+    .sass('scss/main.scss', 'wwwroot/css')
+    .webpackConfig({
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader",
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: ["*", ".js", ".jsx", ".ts", ".tsx"]
+        }
+    });
